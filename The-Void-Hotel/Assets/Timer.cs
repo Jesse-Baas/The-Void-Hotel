11f84c930150ef4ff432f97cd82487a521b4d60c
TwanDalmolem <97114370@st.deltion.nl>
2025-03-04 11:14:43 +0100

    Added canvas with timer
    Added a canvas that contains a TextMeshPro with a script attached to it which makes the TextMeshPro act as a timer.

---
+++ The-Void-Hotel/Assets/Timer.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    public float timer;
+    public float minutes;
+    public float seconds;
+    public TextMeshProUGUI TimerTxt;
+    public bool IsCounting;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (IsCounting == true)
+        {
+            minutes = Mathf.Floor(timer / 60);
+            seconds = Mathf.Floor(timer % 60);
+
+            TimerTxt.text = string.Format("{0:00}:{1:00}", minutes, seconds);
+            timer -= Time.deltaTime;
+            if (timer <= 0)
+            {
+                print("Game Over");
+                IsCounting = false;
+                TimerTxt.text = string.Format("Game Over!");
+            }
+        }
+    }
+}
