509a2d127b7a4586b4b884401170792110f43b0a
TwanDalmolem <97114370@st.deltion.nl>
2025-03-07 15:24:37 +0100

    Added a door-opening feature
    Added a script that allows for objects with the "Door"-tag to be opened and/or closed by pressing "E". Note: Origin Points of doors have to be attached to where the hinge would be or else the doors don't rotate correctly

---
+++ The-Void-Hotel/Assets/DoorOpening.cs
@@ -0,0 +1,41 @@
+using UnityEngine;
+
+public class DoorOpening : MonoBehaviour
+{
+    private float openAngle = 45f;
+    private float closedAngle = 0f;
+    public RaycastHit hit;
+    public float doorRange;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            if (Physics.Raycast(transform.position, transform.forward, out hit, doorRange))
+            {
+                print("Raycast...");
+                if (hit.collider.gameObject.tag == "Door")
+                {
+                    print("Open ne noor!");
+                    Transform Door = hit.collider.transform;
+
+                    if (Mathf.Approximately(Door.eulerAngles.y, openAngle))
+                    {
+                        Door.eulerAngles = new Vector3(Door.eulerAngles.x, closedAngle, Door.eulerAngles.z);
+                    }
+                    else
+                    {
+                        Door.eulerAngles = new Vector3(Door.eulerAngles.x, openAngle, Door.eulerAngles.z);
+                    }
+                }
+            }
+        }
+    }
+}
--- The-Void-Hotel/Assets/Movement.cs
+++ The-Void-Hotel/Assets/Movement.cs
@@ -2,6 +2,14 @@ using UnityEngine;
 
 public class NewMonoBehaviourScript : MonoBehaviour
 {
+    public Vector3 moveDir;
+    public Vector3 rotDir;
+    public Vector3 camRot;
+    public float moveSpeed;
+    public float rotSpeed;
+    public Transform cam;
+    public float sprint;
+    public float sprintSpeed;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -11,6 +19,20 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        moveDir.x = Input.GetAxis("Horizontal");
+        moveDir.z = Input.GetAxis("Vertical");
+        transform.Translate(moveDir * Time.deltaTime * moveSpeed * sprint);
+        rotDir.y = Input.GetAxis("Mouse X");
+        camRot.x = Input.GetAxis("Mouse Y");
+        transform.Rotate(rotDir * Time.deltaTime * rotSpeed);
+        cam.Rotate(-camRot * Time.deltaTime * rotSpeed);
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            sprint = sprintSpeed;
+        }
+        else
+        {
+            sprint = 1;
+        }
     }
 }
