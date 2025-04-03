c30c040981869e92eb778de9bbd00a3f53d0c3c7
TwanDalmolem <97114370@st.deltion.nl>
2025-04-03 20:36:43 +0200

    Re-added Cursor Lock
    Re-added Cursor Lock

--- The-Void-Hotel/Assets/Movement.cs
+++ The-Void-Hotel/Assets/Movement.cs
@@ -13,7 +13,8 @@ public class NewMonoBehaviourScript : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
